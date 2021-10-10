3a6a87b4c889e7423074c152d9598f11caeec67d
-CTDL_GT_K14 <giahaobui810@gmail.com>
2021-10-10 19:38:01 +0700

    Update

--- src/com/haobg2004110027/tuan4/Bai2.java
+++ src/com/haobg2004110027/tuan4/Bai2.java
@@ -1,23 +1,17 @@
 package com.haobg2004110027.tuan4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Bai2 {
+        static  ArrayList<String> studentList = new ArrayList<String>();
+
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
-            ArrayList<String> studenList = new ArrayList<>();
 
-            String name = "", age = "", id = "";
-            
             boolean flag = true;
-            int soLuong;
             int key;
-            
-            do {
-                System.out.print("Nhập số lượng sinh viên: ");
-                soLuong = input.nextInt();
-            } while (soLuong <= 0);
 
             while(flag){
                 showMenu();
@@ -31,36 +25,36 @@ public class Bai2 {
                         break;
                     case 1:
                         System.out.println("Bạn đã chọn [1]");
+                        nhap();
+
+                        do{
+                            System.out.print("Ấn Y để quay lại MENU: ");
+                        } while (input.next().equals("N"));
                         
-                        cls();
-
-                        for(int i = 1; i < soLuong; i++){
-                            System.out.println("\nNhập thông tin sinh viên thứ " + i + ": ");
-                            System.out.print("Tên: ");
-                            name = input.next();
-                            studenList.add(name);
-                            System.out.print("ID: ");
-                            id = input.next();
-                            studenList.add(id);
-                            System.out.print("Tuổi: ");
-                            age = input.next();
-                            studenList.add(age);
-                        }
-
-                        cls();
                         break;
                     case 2:
                         System.out.println("Bạn đã chọn [2]");
-                        System.out.println(studenList);
+                        xuat();
+                        loop();
+
                         break;
                     case 3:
                         System.out.println("Bạn đã chọn [3]");
+                        xuatRand();
+                        loop();
+
                         break;
                     case 4:
                         System.out.println("Bạn đã chọn [4]");
+                        sapxep();
+                        loop();
+
                         break;
                     case 5:
                         System.out.println("Bạn đã chọn [5]");
+                        xoa();
+                        loop();
+
                         break;
                     default:
                         System.out.println("Không có lựa chọn trên!");
@@ -71,34 +65,73 @@ public class Bai2 {
 
         public static void showMenu() {
             System.out.println("=====================================");
-            System.out.println("1. Nhập danh sách họ và tên");
-            System.out.println("2. Xuất danh sách vừa nhập");
-            System.out.println("3. Xuất danh sách ngẫu nhiên");
-            System.out.println("4. Sắp xếp giảm dần và xuất danh sách");
-            System.out.println("5. Tìm và xóa họ tên nhập từ bàn phím");
-            System.out.println("0. Thoát chương trình");
+            System.out.println("[1] Nhập danh sách họ và tên");
+            System.out.println("[2] Xuất danh sách vừa nhập");
+            System.out.println("[3] Xuất danh sách ngẫu nhiên");
+            System.out.println("[4] Sắp xếp giảm dần và xuất danh sách");
+            System.out.println("[5] Tìm và xóa họ tên nhập từ bàn phím");
+            System.out.println("[0] Thoát chương trình");
             System.out.println("=====================================");
         }
-        public static void cls() {
-            System.out.print("\033[H\033[2J");
-            System.out.flush(); 
-        }
-        public static void nhap(String id, String name, String age) {
+
+        public static void loop() {
             Scanner input = new Scanner(System.in);
-            
+
+            do{
+                System.out.print("Ấn Y để quay lại MENU: ");
+            } while (input.next().equals("N"));
 
             input.close();
         }
-        public void xuat() {
-            
+        
+        public static void nhap(){
+            Scanner input = new Scanner(System.in);
+
+                while (true) {
+                    System.out.print("Nhập họ và tên: ");
+                    String name = input.nextLine();
+                    studentList.add(name);
+
+                    System.out.print("Nhập thêm (Y/N): ");
+                    if(input.nextLine().equals("N")){
+                    break;
+                }
+            }
+            input.close();
         }
-        public void xuatRand() {
-            
+
+        public static void xuat(){
+            for (String aString : studentList) {
+                    System.out.println("Họ và tên sinh viên: " + aString);
+            }
         }
-        public void sapxep() {
-            
+
+        public static void sapxep() {
+            Collections.sort(studentList);
+            Collections.reverse(studentList);
+            xuat();
         }
-        public void xoa() {
-            
+
+        public static void xuatRand() {
+            Collections.shuffle(studentList);
+            xuat();
         }
-    }
+
+        public static void xoa() {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Nhập tên cần xóa: ");
+            String name = input.next();
+
+            for(String bString : studentList){
+                if(bString == name){
+                    studentList.remove(bString);
+                    System.out.println("Đã xóa thành công!");
+                } else {
+                    System.out.println("Không tìm thấy tên cần xóa!");
+                }
+            }
+
+            input.close();
+        }
+}
